786c16aef5c153a2d1539c164cd46a76e8f4f31b
JhonEdinson-Shongo <jeca010198@gmail.com>
2025-06-14 22:55:19 -0500

    [FEATURE] Add new animated squares and spacing to animations page

--- designs_1/lib/src/pages/animations_page.dart
+++ designs_1/lib/src/pages/animations_page.dart
@@ -28,6 +28,11 @@ class AnimationsPage extends StatelessWidget {
               alignment: Alignment.centerLeft,
               child: const AnimatedSquareRight(),
             ),
+            const SizedBox(height: 20),
+            const AnimatedSquareRightEnlarge(),
+            const SizedBox(height: 20),
+            const AnimatedSquareMove(),
+            const SizedBox(height: 20),
           ],
         ),
       ),
@@ -247,6 +252,213 @@ class _AnimatedSquareRightState extends State<AnimatedSquareRight>
                   style: TextStyle(fontSize: 25, color: AppTheme.white)),
             ),
           ],
+        );
+      },
+    );
+  }
+}
+
+class AnimatedSquareRightEnlarge extends StatefulWidget {
+  const AnimatedSquareRightEnlarge({super.key});
+  @override
+  State<AnimatedSquareRightEnlarge> createState() =>
+      _AnimatedSquareRightEnlargeState();
+}
+
+class _AnimatedSquareRightEnlargeState extends State<AnimatedSquareRightEnlarge>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animationRotation;
+  Animation<double>? _animationOpacity;
+  Animation<double>? _animationTranslate;
+  Animation<double>? _animationEnlarge;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 3000));
+
+    _animationRotation = Tween<double>(begin: 0, end: 2 * Math.pi).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _animationOpacity = Tween<double>(begin: 0.2, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: const Interval(0, 1, curve: Curves.easeIn),
+        reverseCurve: const Interval(0, 1, curve: Curves.easeIn),
+      ),
+    );
+
+    _animationTranslate = Tween<double>(begin: 0, end: 1).animate(
+        CurvedAnimation(
+            parent: _controller!,
+            curve: const Interval(0, 1, curve: Curves.easeInOut)));
+
+    _animationEnlarge =
+        Tween<double>(begin: 0.2, end: 1).animate(CurvedAnimation(
+      parent: _controller!,
+      curve: const Interval(0, 1, curve: Curves.easeInOut),
+      reverseCurve: const Interval(0, 1, curve: Curves.easeInOut),
+    ));
+
+    _controller!.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller!.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        _controller!.forward();
+      }
+    });
+    if (mounted) {
+      _controller!.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller!,
+      child: _Rectangle(),
+      builder: (context, child) {
+        return Stack(
+          alignment: Alignment.center,
+          children: [
+            Transform.scale(
+              scale: _animationEnlarge!.value,
+              child: Transform.translate(
+                offset: Offset(
+                  _animationTranslate!.value *
+                      (MediaQuery.of(context).size.width - 120),
+                  0,
+                ),
+                child: Transform.rotate(
+                  angle: _animationRotation!.value,
+                  child: Opacity(
+                    opacity: _animationOpacity!.value,
+                    child: child,
+                  ),
+                ),
+              ),
+            ),
+            Transform.scale(
+              scale: _animationEnlarge!.value,
+              child: Transform.translate(
+                offset: Offset(
+                  _animationTranslate!.value *
+                      (MediaQuery.of(context).size.width - 120),
+                  0,
+                ),
+                child: const Text('Jhon',
+                    style: TextStyle(fontSize: 25, color: AppTheme.white)),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class AnimatedSquareMove extends StatefulWidget {
+  const AnimatedSquareMove({super.key});
+  @override
+  State<AnimatedSquareMove> createState() => _AnimatedSquareMoveState();
+}
+
+class _AnimatedSquareMoveState extends State<AnimatedSquareMove>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animationTranslateRight;
+  Animation<double>? _animationTranslateLeft;
+  Animation<double>? _animationTranslateUp;
+  Animation<double>? _animationTranslateDown;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 5000));
+
+    _animationTranslateRight = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: const Interval(0, 0.25, curve: Curves.decelerate),
+      ),
+    );
+    _animationTranslateDown = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: const Interval(0.25, 0.5, curve: Curves.bounceOut),
+      ),
+    );
+    _animationTranslateLeft = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: const Interval(0.5, 0.75, curve: Curves.decelerate),
+      ),
+    );
+    _animationTranslateUp = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: const Interval(0.75, 1, curve: Curves.bounceOut),
+      ),
+    );
+
+    _controller!.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller!.reset();
+      } else if (status == AnimationStatus.dismissed) {
+        _controller!.forward();
+      }
+    });
+    if (mounted) {
+      _controller!.forward();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _controller!,
+      child: _Rectangle(),
+      builder: (context, child) {
+        return Container(
+          color: Colors.black.withOpacity(0.1),
+          width: MediaQuery.of(context).size.width,
+          alignment: Alignment.topLeft,
+          height: 160,
+          child: Transform.translate(
+            offset: Offset(
+              (_animationTranslateRight!.value -
+                      _animationTranslateLeft!.value) *
+                  (MediaQuery.of(context).size.width - 120),
+              (_animationTranslateDown!.value - _animationTranslateUp!.value) * 80,
+            ),
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(child: child),
+                const Text('Jhon',
+                    style: TextStyle(fontSize: 25, color: AppTheme.white)),
+              ],
+            ),
+          ),
         );
       },
     );
