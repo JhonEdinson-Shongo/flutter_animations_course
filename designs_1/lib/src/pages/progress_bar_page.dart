427628bc6d32a0421b42cd56de34d54fc4ef35bb
JhonEdinson-Shongo <jeca010198@gmail.com>
2025-06-24 14:44:09 -0500

    [FEAT] end section radial progress

--- designs_1/lib/src/pages/progress_bar_page.dart
+++ designs_1/lib/src/pages/progress_bar_page.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:designs_1/src/themes/app_theme.dart';
 import 'package:designs_1/src/widgets/radial_progress.dart';
 import 'package:flutter/material.dart';
@@ -13,50 +11,7 @@ class CustomProgressPage extends StatefulWidget {
 
 class _CustomProgressPageState extends State<CustomProgressPage>
     with SingleTickerProviderStateMixin {
-  AnimationController? _controller;
-  Animation<double>? _animationProgress;
   double progress = 0.0;
-  double tempProgress = 0.0;
-  double increment = 0.1;
-  bool isLoading = false;
-
-  @override
-  void initState() {
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 1000),
-    );
-
-    _animationProgress = Tween(begin: 0.0, end: 1.0).animate(
-      CurvedAnimation(
-        parent: _controller!,
-        curve: Curves.easeInOutBack,
-      ),
-    );
-
-    _controller?.addListener(() {
-      setState(() {
-        isLoading = true;
-        progress = tempProgress + (_animationProgress!.value * increment);
-      });
-    });
-
-    _controller?.addStatusListener((status) {
-      setState(() {
-        if (status == AnimationStatus.completed) {
-          isLoading = false;
-          increment = 0.1;
-        }
-      });
-    });
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller?.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -79,19 +34,69 @@ class _CustomProgressPageState extends State<CustomProgressPage>
               Text('Circular Progress custom',
                   style: Theme.of(context).textTheme.titleLarge),
               const SizedBox(height: 20.0),
-              Container(
-                width: 200.0,
-                height: 200.0,
-                color: AppTheme.black.withOpacity(0.03),
-                child: CustomPaint(
-                  painter: _CustomRadialProgress(progress, strokeWidth: 10.0),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: MediaQuery.of(context).size.width * 0.4,
+                    child: RadialProgress(
+                      progress: progress,
+                      stroke: 7.0,
+                      secondStroke: 3.0,
+                      animationDuration: 1000,
+                      colorProgress: AppTheme.primary,
+                      colorBackground: Colors.grey,
+                      curve: Curves.easeInOutQuart,
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: MediaQuery.of(context).size.width * 0.4,
+                    child: RadialProgress(
+                      progress: progress,
+                      stroke: 7.0,
+                      secondStroke: 3.0,
+                      animationDuration: 1000,
+                      colorProgress: AppTheme.secondary,
+                      colorBackground: Colors.grey,
+                      curve: Curves.easeInOutQuart,
+                    ),
+                  ),
+                ],
               ),
               const SizedBox(height: 20.0),
-              Text('Circular Progress custom',
-                  style: Theme.of(context).textTheme.titleLarge),
-              const SizedBox(height: 20.0),
-              RadialProgress(progress: progress),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: MediaQuery.of(context).size.width * 0.4,
+                    child: RadialProgress(
+                      progress: progress,
+                      stroke: 7.0,
+                      secondStroke: 3.0,
+                      animationDuration: 1000,
+                      colorProgress: Colors.teal,
+                      colorBackground: Colors.grey,
+                      curve: Curves.easeInOutQuart,
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.4,
+                    height: MediaQuery.of(context).size.width * 0.4,
+                    child: RadialProgress(
+                      progress: progress,
+                      stroke: 7.0,
+                      secondStroke: 3.0,
+                      animationDuration: 1000,
+                      colorProgress: Colors.purple,
+                      colorBackground: Colors.grey,
+                      curve: Curves.easeInOutQuart,
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
@@ -101,89 +106,29 @@ class _CustomProgressPageState extends State<CustomProgressPage>
         children: [
           FloatingActionButton(
             child: const Icon(Icons.arrow_circle_up_sharp),
-            onPressed: () {
-              if (isLoading) return;
-              setState(() {
-                tempProgress = progress;
-                if (tempProgress >= 1.0) {
-                  tempProgress = 0.0;
-                  progress = 0.0;
-                }
-                _controller?.forward(from: 0.0);
-              });
-            },
+            onPressed: () => setState(() {
+              print(progress);
+              if (progress >= 0.99) return;
+              progress += 0.1;
+            }),
           ),
           const SizedBox(height: 10.0),
           FloatingActionButton(
             child: const Icon(Icons.restart_alt),
-            onPressed: () {
-              if (isLoading) return;
-              tempProgress = progress;
-              setState(() {
-                increment = -progress;
-              });
-              _controller?.forward(from: 0.0);
-            },
+            onPressed: () => setState(() {
+              progress = 0.0;
+            }),
           ),
           const SizedBox(height: 10.0),
           FloatingActionButton(
             child: const Icon(Icons.arrow_circle_down_sharp),
-            onPressed: () {
-              if (isLoading || progress == 0.0) return;
-              setState(() {
-                tempProgress = progress;
-                increment = increment.abs() * -1;
-                if (tempProgress <= 0.0) {
-                  tempProgress = 0.0;
-                  progress = 0.0;
-                }
-                _controller?.forward(from: 0.0);
-              });
-            },
+            onPressed: () => setState(() {
+              if (progress <= 0.0) return;
+              progress -= 0.1;
+            }),
           ),
         ],
       ),
     );
   }
-}
-
-class _CustomRadialProgress extends CustomPainter {
-  final double progress;
-  final double strokeWidth;
-
-  _CustomRadialProgress(this.progress, {this.strokeWidth = 10.0});
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()
-      ..color = Colors.grey
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = strokeWidth / 2;
-
-    Offset center = Offset(size.width * 0.5, size.height * 0.5);
-    double radius = min(
-      size.width * 0.5 - strokeWidth / 2,
-      size.height * 0.5 - strokeWidth / 2,
-    );
-
-    canvas.drawCircle(center, radius, paint);
-
-    double arcAngle = 2 * pi * progress;
-
-    final paintArc = Paint()
-      ..color = AppTheme.primary
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = strokeWidth;
-
-    canvas.drawArc(
-      Rect.fromCircle(center: center, radius: radius),
-      -pi / 2,
-      arcAngle,
-      false,
-      paintArc,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
--- designs_1/lib/src/widgets/radial_progress.dart
+++ designs_1/lib/src/widgets/radial_progress.dart
@@ -1,19 +1,166 @@
+import 'dart:math';
+import 'package:designs_1/src/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 class RadialProgress extends StatefulWidget {
   final double progress;
+  final double stroke;
+  final double secondStroke;
+  final Color colorProgress;
+  final Color colorBackground;
+  final Curve curve;
+  final int animationDuration;
+
   const RadialProgress({
     super.key,
     required this.progress,
+    this.stroke = 10.0,
+    this.colorProgress = AppTheme.primary,
+    this.colorBackground = Colors.grey,
+    this.curve = Curves.linear,
+    this.secondStroke = 0.0,
+    this.animationDuration = 500,
   });
 
   @override
   State<RadialProgress> createState() => _RadialProgressState();
 }
 
-class _RadialProgressState extends State<RadialProgress> {
+class _RadialProgressState extends State<RadialProgress>
+    with SingleTickerProviderStateMixin {
+  AnimationController? _controller;
+  Animation<double>? _animationProgress;
+  double progress = 0.0;
+  double tempProgress = 0.0;
+  double increment = 0.0;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    progress = widget.progress;
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: widget.animationDuration),
+    );
+
+    _animationProgress = Tween(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller!,
+        curve: widget.curve,
+      ),
+    );
+
+    _controller?.addListener(() {
+      setState(() {
+        isLoading = true;
+        progress = tempProgress + (_animationProgress!.value * increment);
+      });
+    });
+
+    _controller?.addStatusListener((status) {
+      setState(() {
+        if (status == AnimationStatus.completed) {
+          isLoading = false;
+          tempProgress = progress;
+        }
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Progress: ${widget.progress}');
+    setState(() {
+      increment = widget.progress - tempProgress;
+      if (widget.progress < 0.0 || widget.progress > 1.0) return;
+      if (isLoading || progress == widget.progress) return;
+      _controller?.forward(from: 0.0);
+    });
+    return SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child: CustomPaint(
+        painter: _CustomRadialProgress(
+          progress,
+          widget.stroke,
+          widget.colorProgress,
+          widget.colorBackground,
+          widget.secondStroke > 0.0 ? widget.secondStroke : widget.stroke / 2,
+        ),
+      ),
+    );
   }
+}
+
+class _CustomRadialProgress extends CustomPainter {
+  final double progress;
+  final double strokeWidth;
+  final double secondStrokeWidth;
+  final Color colorProgress;
+  final Color colorBackground;
+
+  _CustomRadialProgress(
+    this.progress,
+    this.strokeWidth,
+    this.colorProgress,
+    this.colorBackground,
+    this.secondStrokeWidth,
+  );
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // final Rect rect = Rect.fromCircle(
+    //   center: Offset(size.width * 0.5, size.height * 0.5),
+    //   radius: size.width*0.5,
+    // );
+
+    // const Gradient gradient = SweepGradient(
+    //   colors: [
+    //     Colors.green,
+    //     Colors.blue,
+    //     Colors.yellow,
+    //     Colors.red,
+    //   ],
+    //   transform: GradientRotation(-pi / 2),
+    // );
+
+    final paint = Paint()
+      ..color = colorBackground
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = secondStrokeWidth;
+
+    Offset center = Offset(size.width * 0.5, size.height * 0.5);
+    double radius = min(
+      size.width * 0.5 - secondStrokeWidth,
+      size.height * 0.5 - secondStrokeWidth,
+    );
+
+    canvas.drawCircle(center, radius, paint);
+
+    double arcAngle = 2 * pi * progress;
+
+    final paintArc = Paint()
+      ..color = colorProgress
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = strokeWidth
+      ..strokeCap = StrokeCap.round;
+      // ..shader = gradient.createShader(rect);
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radius),
+      -pi / 2,
+      arcAngle,
+      false,
+      paintArc,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
 }
